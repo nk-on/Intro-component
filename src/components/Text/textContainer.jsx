bff656d3a79ac4b54201fea35d88c3701a7ba6c7
nk-on <johndoe@example.com>
2024-11-19 23:01:07 +0400

    Styling text

--- src/components/Text/textContainer.jsx
+++ src/components/Text/textContainer.jsx
@@ -1,7 +1,7 @@
 export default function TextContainer() {
   return (
-    <div className="Text-Container">
-      <h1>Learn to code by watching others</h1>
+    <div className="text-white w-[525px]">
+      <h1 className="font-bold text-[50px]">Learn to code by watching others</h1>
       <p>
         See how experienced developers solve problems in real-time. Watching
         scripted tutorials is great, but understanding how developers think is
