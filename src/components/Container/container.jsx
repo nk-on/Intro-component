84e386efb3645022db5f896c746e8012bd6d2e05
nk-on <johndoe@example.com>
2024-11-19 23:00:28 +0400

    Style:adding boxshadow to form container

--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -1,5 +1,5 @@
 export default function Container(){
-    return <div className="bg-white w-3/4 md:w-[540px] h-[474px]">
+    return <div className="bg-white w-3/4 md:w-[540px] h-[474px] shadow-container-shadow">
 
     </div>
 }
