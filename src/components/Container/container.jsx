d42dec6bfafa102368d855785802dfeb194cff39
nk-on <johndoe@example.com>
2024-11-20 21:40:15 +0400

    Style:adding and styling content inside container

--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -1,5 +1,16 @@
 export default function Container(){
-    return <div className="bg-white w-3/4 md:w-[540px] h-[474px] shadow-container-shadow">
-
+    return <div className="flex flex-col justify-center	 gap-3 w-[50%]" >
+        <div className="w-[90vw] h-[60px] md:w-[540px]  bg-[#5E54A4]">
+            <p className="text-white flex justify-center items-center h-full">Try it free 7 days then $20/mo. thereafter</p>
+        </div>
+        <div className="w-[90vw]  h-[474px] md:w-[540px]  shadow-container-shadow bg-white">
+            <form className="flex gap-[20px] flex-col justify-center items-center w-full h-full">
+                <input type="text" placeholder="First Name" className="w-[90%] h-[56px] rounded-[5px] border border-black-500 " />
+                <input type="text" placeholder="Last Name" className="w-[90%] h-[56px] rounded-[5px] border border-black-500" />
+                <input type="email" placeholder="Email" className="w-[90%] h-[56px] rounded-[5px] border border-black-500" />
+                <input type="password" placeholder="Password" className="w-[90%] h-[56px] rounded-[5px] border border-black-500" />
+                <button className="bg-[#38CC8B] w-[90%] h-[56px] rounded-[5px]">CLAIM YOUR FREE TRIAL</button>
+            </form>
+        </div>
     </div>
 }
--- src/components/Text/textContainer.jsx
+++ src/components/Text/textContainer.jsx
@@ -1,7 +1,7 @@
 export default function TextContainer() {
   return (
-    <div className="text-white w-[525px]">
-      <h1 className="font-bold text-[50px]">Learn to code by watching others</h1>
+    <div className="text-white w-1/4 md:[525px]">
+      <h1 className="font-bold text-[50px] md:text-[28px]">Learn to code by watching others</h1>
       <p>
         See how experienced developers solve problems in real-time. Watching
         scripted tutorials is great, but understanding how developers think is
